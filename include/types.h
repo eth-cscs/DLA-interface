f7fe729f0af672c92aef90bf1958dcbd1217dc5a
Raffaele Solcà <rasolca@cscs.ch>
2017-07-20 14:04:27 +0200

    Added SizeType and IndexType types. Added SolverType and DistributionType enums

--- include/types.h
+++ include/types.h
@@ -2,11 +2,22 @@
 #define DLA_INTERFACE_TYPES_H
 
 namespace dla_interface {
+  enum SolverType { scalapack = 1, elpa = 2, dplasma = 3, chameleon = 4 };
+
+  // Distributed matrix distribution (see documentation for details):
+  // Scalapack = 2D block cyclic distribution,
+  // Tile = 2D block cyclic tile distribution.
+  enum DistributionType { scalapack_dist = 1, tile_dist = 2 };
+
+  using IndexType = int;
+  using SizeType = int;
+
 #ifdef DLA_HAVE_SCALAPACK
+  using ScalapackIndex = int;  // Base 1 indeces.
   using ScalapackDescriptor = int*;
   using BlacsContextType = int;
 #endif
-#ifdef HAVE_DPLASMA
+#ifdef DLA_HAVE_DPLASMA
   using DPlasmaDescriptor = two_dim_block_cyclic_t;
 #endif
 
