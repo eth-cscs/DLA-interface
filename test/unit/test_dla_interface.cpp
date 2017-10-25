d21814291705b1a43e022e43d0c8cf7aab383fb4
Raffaele Solcà <rasolca@cscs.ch>
2017-10-25 14:00:39 +0200

    Cholesky factorization test with ia and ja != 1

--- test/unit/test_dla_interface.cpp
+++ test/unit/test_dla_interface.cpp
@@ -49,14 +49,20 @@ TYPED_TEST(DLATypedTest, CholeskyFactorization) {
     for (auto dist : {scalapack_dist, tile_dist}) {
       for (auto solver : {ScaLAPACK}) {
         for (auto uplo : {Lower, Upper}) {
-          DistributedMatrix<ElType> A(n, n, nb, nb, *comm_ptr, dist);
-          fillDistributedMatrix(A, el_val);
+          auto A1 = std::make_shared<DistributedMatrix<ElType>>(n, n, nb, nb, *comm_ptr, dist);
+          auto A2 =
+              DistributedMatrix<ElType>(n + nb, n + nb, nb, nb, *comm_ptr, dist).subMatrix(n, n, nb, nb);
+          for (auto A_ptr : {A1, A2}) {
+            auto& A = *A_ptr;
+            fillDistributedMatrix(A, el_val);
 
-          choleskyFactorization(uplo, A, solver);
-          if (uplo == Lower)
-            EXPECT_TRUE(checkNearDistributedMatrix(A, el_val_expected_lower, 10 * this->epsilon()));
-          if (uplo == Upper)
-            EXPECT_TRUE(checkNearDistributedMatrix(A, el_val_expected_upper, 10 * this->epsilon()));
+            choleskyFactorization(uplo, A, solver);
+
+            if (uplo == Lower)
+              EXPECT_TRUE(checkNearDistributedMatrix(A, el_val_expected_lower, 10 * this->epsilon()));
+            if (uplo == Upper)
+              EXPECT_TRUE(checkNearDistributedMatrix(A, el_val_expected_upper, 10 * this->epsilon()));
+          }
         }
       }
     }
