5ff786d3e837973c895fef8363ef8e163c69cd5b
Raffaele Solcà <rasolca@cscs.ch>
2017-11-15 19:34:18 +0100

    Fixed const correctness

--- include/scalapack.h
+++ include/scalapack.h
@@ -14,12 +14,14 @@ namespace scalapack {
   void descset_(int* desc, const int* m, const int* n, const int* mb, const int* nb,
                 const int* irsrc, const int* icsrc, const int* ictxt, const int* lld);
 
-  void pspotrf_(const char* uplo, const int* n, float* a, int* ia, int* ja, int* desca, int* info);
-  void pdpotrf_(const char* uplo, const int* n, double* a, int* ia, int* ja, int* desca, int* info);
-  void pcpotrf_(const char* uplo, const int* n, std::complex<float>* a, int* ia, int* ja,
-                int* desca, int* info);
-  void pzpotrf_(const char* uplo, const int* n, std::complex<double>* a, int* ia, int* ja,
-                int* desca, int* info);
+  void pspotrf_(const char* uplo, const int* n, float* a, const int* ia, const int* ja,
+                const int* desca, int* info);
+  void pdpotrf_(const char* uplo, const int* n, double* a, const int* ia, const int* ja,
+                const int* desca, int* info);
+  void pcpotrf_(const char* uplo, const int* n, std::complex<float>* a, const int* ia,
+                const int* ja, const int* desca, int* info);
+  void pzpotrf_(const char* uplo, const int* n, std::complex<double>* a, const int* ia,
+                const int* ja, const int* desca, int* info);
   }
 }
 #endif
