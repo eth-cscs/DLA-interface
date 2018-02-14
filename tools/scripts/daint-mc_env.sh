392fc37326c6b3e34aa51eb56852f6280ca69fed
Raffaele Solcà <rasolca@cscs.ch>
2018-02-14 19:16:58 +0100

    Added Jenkins scripts

---
+++ tools/scripts/daint-mc_env.sh
@@ -0,0 +1,10 @@
+module switch PrgEnv-cray PrgEnv-gnu
+module load daint-mc        
+module load CMake
+module load cray-python
+module unload cray-libsci   # should have no effect but it makes sure that libsci is not used if MKL is linked incorrectly.
+module load intel           # defines $MKLROOT
+module load hwloc
+module load Boost
+
+export CRAYPE_LINK_TYPE=dynamic
