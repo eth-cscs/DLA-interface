065b1aa8690ea946e103b999c56911b320008451
Raffaele Solcà <rasolca@cscs.ch>
2018-03-22 16:25:06 +0100

    added output stream argument to matrix elements test functions

--- test/include/util_local_matrix.h
+++ test/include/util_local_matrix.h
@@ -12,19 +12,24 @@ namespace testing {
     }
   }
 
-  template <class Type, class F>
-  bool checkLocalMatrix(const LocalMatrix<Type>& mat, F& el_val) {
+  template <class Type, class F, class Out>
+  bool checkLocalMatrix(const LocalMatrix<Type>& mat, F& el_val, Out& out) {
     for (int j = 0; j < mat.size().second; ++j) {
       for (int i = 0; i < mat.size().first; ++i) {
         if (mat(i, j) != el_val(i, j)) {
-          std::cout << "Element (" << i << ", " << j << ") is wrong." << std::endl;
-          std::cout << "Expected " << el_val(i, j) << "," << std::endl;
-          std::cout << "Got " << mat(i, j) << "." << std::endl;
+          out << "Element (" << i << ", " << j << ") is wrong." << std::endl;
+          out << "Expected " << el_val(i, j) << "," << std::endl;
+          out << "Got " << mat(i, j) << "." << std::endl;
           return false;
         }
       }
     }
     return true;
+  }
+
+  template <class Type, class F>
+  bool checkLocalMatrix(const LocalMatrix<Type>& mat, F& el_val) {
+    return checkLocalMatrix(mat, el_val, std::cout);
   }
 }
 
