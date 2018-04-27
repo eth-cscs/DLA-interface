7788608696c14f7b4c2c1104b4b696371376e605
Raffaele Solcà <rasolca@cscs.ch>
2018-04-27 11:20:18 +0200

    Added miniapp for matrix matrix multiplication

--- cmake/dla_miniapp.cmake
+++ cmake/dla_miniapp.cmake
@@ -1,15 +1,20 @@
-# Usage: add_test(test_name [MPIRANKS <np>] [SRC <file.cpp>] [DEFS <defs>][LIBS <libs>])
-# If SRC is not specified  ${test_name}.cpp is used.
+# Usage: add_miniapp(miniapp_name [MPIRANKS <np>] [SRC <file.cpp>] [DEFS <defs>][LIBS <libs>])
+# If EXE_NAME is not specified  ${miniapp_name}.cpp is used.
+# If SRC is not specified  ${EXEC_NAME}.cpp is used.
 # DEFS are the extra definitions needed to compile the test.
-# If MPIRANKS is defined the test is linked with the MPI library and runned with
-# ${TEST_RUNNER} ${RUNNER_NP_OPT} ${np} <exe>
 function(add_miniapp name)
-  cmake_parse_arguments("arg" "" "SRC" "DEFS;LIBS" ${ARGN} )
+  cmake_parse_arguments("arg" "" "EXE_NAME;SRC" "DEFS;LIBS" ${ARGN} )
+
+  if(DEFINED arg_EXE_NAME)
+    set(exe ${arg_EXE_NAME})
+  else()
+    set(exe ${name})
+  endif()
 
   if(DEFINED arg_SRC)
     set(source ${arg_SRC})
   else()
-    set(source ${name}.cpp)
+    set(source ${exe}.cpp)
   endif()
 
   add_executable(${name} ${source})
@@ -18,5 +23,6 @@ function(add_miniapp name)
     target_compile_definitions(${name} PRIVATE ${arg_DEFS})
   endif()
   target_link_libraries(${name} gtest dla_interface ${arg_LIBS})
+  set_target_properties(${name} PROPERTIES OUTPUT_NAME ${exe})
 
 endfunction()
