6eb549d5747e59142c58930f153fd135fa59170b
Alberto Invernizzi <invernizzi@cscs.ch>
2021-03-22 16:58:06 +0100

    minor changes to findscalapack

--- cmake/FindHWLOC.cmake
+++ cmake/FindHWLOC.cmake
@@ -1,9 +1,26 @@
+#
+# Distributed Linear Algebra Interface (DLAI)
+#
+# Copyright (c) 2018-2021, ETH Zurich
+# All rights reserved.
+#
+# Please, refer to the LICENSE file in the root directory.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# Find HWLOC library
+#
+# If HWLOC_ROOT is set, either in the environment or as CMake cache variable, it will
+# be used for searching the library. Otherwise, pkg-config will be used.
+#
+# If found, this module creates the CMake target HWLOC::HWLOC
+
 if (DEFINED HWLOC_ROOT OR DEFINED ENV{HWLOC_ROOT})
   find_path(HWLOC_INCLUDE_DIRS hwloc.h REQUIRED)
   find_library(HWLOC_LIBRARIES hwloc REQUIRED)
 else()
   find_package(PkgConfig REQUIRED QUIET)
-  pkg_search_module(HWLOC hwloc IMPORTED_TARGET GLOBAL)
+  pkg_search_module(HWLOC hwloc)
 endif()
 
 include(FindPackageHandleStandardArgs)
