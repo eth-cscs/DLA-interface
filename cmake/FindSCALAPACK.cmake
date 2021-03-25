6a3f514349fec126e3ace98a7e0cdf7474bbb7cd
Alberto Invernizzi <invernizzi@cscs.ch>
2021-03-25 11:32:01 +0100

    use default FindLAPACK and try to mimic it in FindSCALAPACK

--- cmake/FindSCALAPACK.cmake
+++ cmake/FindSCALAPACK.cmake
@@ -1,81 +1,123 @@
 #
-# Distributed Linear Algebra Interface (DLAI)
+# CMake recipes
 #
 # Copyright (c) 2018-2021, ETH Zurich
-# All rights reserved.
+# BSD 3-Clause License. All rights reserved.
 #
-# Please, refer to the LICENSE file in the root directory.
-# SPDX-License-Identifier: BSD-3-Clause
+# author: Alberto Invernizzi (a.invernizzi@cscs.ch)
 #
 
 # Find SCALAPACK library
 #
-# This module finds an installed library that implements the SCALAPACK linear-algebra interface.
+# !!! WARNING !!!
+# It is up to the user to ensure the compatibility between the ScaLAPACK library and any
+# other BLAS/LAPACK and MPI implementations used in the project.
 #
-# This module sets the following variables:
-#  SCALAPACK_FOUND - set to true if a library implementing the SCALAPACK interface is found
+# This module checks if `netlib-scalapack` is available or if the compiler implicitly links
+# to a ScaLAPACK implementation.
 #
-# Following options are allowed (for setting options from code see next section):
-#   SCALAPACK_TYPE - it can be "Compiler" or "Custom"
-#     - Compiler (Default): The compiler add the scalapack flag automatically therefore no
-#                           extra link line has to be added.
-#     - Custom: User can specify include folders and libraries through
-#   SCALAPACK_INCLUDE_DIR - used if SCALAPACK_TYPE=Custom
-#       ;-list of include paths
-#   SCALAPACK_LIBRARY - used if SCALAPACK_TYPE=Custom
+# Users can manually specify next variables (even by setting them empty to force use of
+# the compiler implicit linking) to control which implementation they want to use:
+#   SCALAPACK_LIBRARY
 #       ;-list of {lib name, lib filepath, -Llibrary_folder}
 #
-# If you want to set options from CMake script you cannot use previous variables, instead you should use:
-#   - SCALAPACK_CUSTOM_TYPE
-#   - SCALAPACK_CUSTOM_INCLUDE_DIR
-#   - SCALAPACK_CUSTOM_LIBRARY
+# This module sets the following variables:
+#   SCALAPACK_FOUND - set to true if a library implementing the SCALAPACK interface is found
 #
-# It creates target SCALAPACK::SCALAPACK
+# If ScaLAPACK symbols got found, it creates target SCALAPACK::SCALAPACK
 
 cmake_minimum_required(VERSION 3.12)
 
-### Detect
+macro(_scalapack_find_dependency dep)
+  set(_scalapack_quiet_arg)
+  if(SCALAPACK_FIND_QUIETLY)
+    set(_scalapack_quiet_arg QUIET)
+  endif()
+  set(_scalapack_required_arg)
+  if(SCALAPACK_FIND_REQUIRED)
+    set(_scalapack_required_arg REQUIRED)
+  endif()
+  find_package(${dep} ${ARGN}
+    ${_scalapack_quiet_arg}
+    ${_scalapack_required_arg})
+  if (NOT ${dep}_FOUND)
+    set(SCALAPACK_NOT_FOUND_MESSAGE "SCALAPACK could not be found because dependency ${dep} could not be found.")
+  endif()
 
-find_library(SCALAPACK_LIBRARY
-  NAMES
-    scalapack # netlib-scalapack
-)
+  set(_scalapack_required_arg)
+  set(_scalapack_quiet_arg)
+endmacro()
 
-mark_as_advanced(
-  SCALAPACK_LIBRARY
-)
+macro(_scalapack_check_symbols _PREFIX)
+  include(CMakePushCheckState)
+  cmake_push_check_state(RESET)
 
-# ===== TEST
-include(CMakePushCheckState)
-cmake_push_check_state(RESET)
+  set(_libraries ${${_PREFIX}_LIBRARY})
 
-set(CMAKE_REQUIRED_LIBRARIES ${SCALAPACK_LIBRARY})
+  set(CMAKE_REQUIRED_QUIET TRUE)
+  set(CMAKE_REQUIRED_LIBRARIES "${_libraries}")
 
-include(CheckFunctionExists)
+  include(CheckFunctionExists)
 
-unset(_SCALAPACK_CHECK CACHE)
-check_function_exists(pdpotrf_ _SCALAPACK_CHECK)
+  unset(_SCALAPACK_CHECK CACHE)
+  check_function_exists(pdpotrf_ _SCALAPACK_CHECK)
 
-unset(_SCALAPACK_CHECK_BLACS CACHE)
-check_function_exists(Cblacs_exit _SCALAPACK_CHECK_BLACS)
+  unset(_SCALAPACK_CHECK_BLACS CACHE)
+  check_function_exists(Cblacs_exit _SCALAPACK_CHECK_BLACS)
 
-cmake_pop_check_state()
+  cmake_pop_check_state()
 
-### Package
+  if (_SCALAPACK_CHECK AND _SCALAPACK_CHECK_BLACS)
+    if (_libraries)
+      set(SCALAPACK_LIBRARY ${_libraries})
+    else()
+      set(SCALAPACK_LIBRARY "SCALAPACK-PLACEHOLDER-FOR-EMPTY-LIBRARIES")
+    endif()
+  else()
+    set(SCALAPACK_LIBRARY FALSE)
+  endif()
+
+  unset(_libraries)
+endmacro()
+
+
+_scalapack_find_dependency(LAPACK)
+
+if (NOT SCALAPACK_NOT_FOUND_MESSAGE)
+  # check if compiler implicitly links to SCALAPACK...
+  # ... or use what the users specified in SCALAPACK_LIBRARY
+  _scalapack_check_symbols("SCALAPACK")
+
+  # netlib-scalapack
+  if (NOT SCALAPACK_LIBRARY)
+    find_library(netlib_LIBRARY NAMES scalapack)
+    _scalapack_check_symbols("netlib")
+  endif()
+endif()
+
+if (SCALAPACK_NOT_FOUND_MESSAGE)
+  set(SCALAPACK_NOT_FOUND_MESSAGE REASON_FAILURE_MESSAGE "${SCALAPACK_NOT_FOUND_MESSAGE}")
+endif()
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SCALAPACK DEFAULT_MSG
-  _SCALAPACK_CHECK_BLACS
-  _SCALAPACK_CHECK
-)
+find_package_handle_standard_args(SCALAPACK
+  REQUIRED_VARS SCALAPACK_LIBRARY
+  ${SCALAPACK_NOT_FOUND_MESSAGE})
 
+# In case compiler implicitly links, remove empty variable placeholder
+if (SCALAPACK_LIBRARY MATCHES "SCALAPACK-PLACEHOLDER-FOR-EMPTY-LIBRARIES")
+  set(SCALAPACK_LIBRARY)
+endif()
+
+# Create the CMake target
 if (SCALAPACK_FOUND)
   if (NOT TARGET SCALAPACK::SCALAPACK)
     add_library(SCALAPACK::SCALAPACK INTERFACE IMPORTED)
   endif()
 
+  # note: if the compiler implicitly link to SCALAPACK
   if (SCALAPACK_LIBRARY)
     set_target_properties(SCALAPACK::SCALAPACK PROPERTIES
-      IMPORTED_LOCATION "${SCALAPACK_LIBRARY}"
-    )
+      IMPORTED_LOCATION ${SCALAPACK_LIBRARY})
   endif()
 endif()
